e686e062319fe0393780f2b9a95d1f3d6101308a
rehanmirza76 <abdulrehan0317@gmail.com>
2024-11-09 14:14:07 +0500

    update

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useRef, useEffect } from 'react';
+import Typed from 'typed.js';
 
 export default function Hero() {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["I'm Abdul Rehan",' A Website Developer',' A UI/UX Designer'],
+      typeSpeed: 50,
+    });
+
+    // Cleanup function to destroy Typed instance
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div>
       <div className="hero">
-      <div className="left">
-        Hi! I am <br />
-        Abdul Rehan
-        <p>Creating digital products and experiences for all-kind of desires</p>
-      </div>
-      <div className="right">
-        <img src="/Designer-bro.png" alt="" />
+        <div className="left">
+          Hi! <br />
+          <span ref={el} />
+          <p>Creating digital products and experiences for all kinds of desires</p>
+        </div>
+        <div className="right">
+          <img src="/Designer-bro.png" alt="" />
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
