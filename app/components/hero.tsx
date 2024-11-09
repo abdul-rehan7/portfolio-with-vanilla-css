cbcdc1cd60f0bfb1f4a12942b5d984b31e92ff12
rehanmirza76 <abdulrehan0317@gmail.com>
2024-11-09 13:12:48 +0500

    update

---
+++ app/components/hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function hero() {
+  return (
+    <div>
+      <div className="hero">
+      <div className="left">
+        Hi! I am <br />
+        Abdul Rehan
+        <p>Creating digital products and experiences for all-kind of desires</p>
+      </div>
+      <div className="right">
+        <img src="/images/Designer-bro.png" alt="" />
+      </div>
+    </div>
+    </div>
+  )
+}
